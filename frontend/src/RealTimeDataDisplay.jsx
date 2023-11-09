a82e0fcfa49e279b5b30c2801f85523fe3106c94
Pritam Das <dpritam28577@gmail.com>
2023-11-09 11:41:28 +0530

    Remove RealTimeDataDisplay component.

---
+++ frontend/src/RealTimeDataDisplay.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+
+const ENDPOINT = 'http://localhost:5173'; // Replace with your backend endpoint
+
+const RealTimeDataDisplay = () => {
+  const [realTimeData, setRealTimeData] = useState([]);
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on('validatedData', (data) => {
+      // Data received from the backend
+      setRealTimeData((prevData) => [...prevData, data]);
+    });
+
+    return () => socket.disconnect();
+  }, []);
+
+  return (
+    <div>
+      <h2>Real-time Data Display</h2>
+      <ul>
+        {realTimeData.map((data, index) => (
+          <li key={index}>
+            Name: {data.name}, Origin: {data.origin}, Destination: {data.destination}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RealTimeDataDisplay;
